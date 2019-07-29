822138982f945ee581a0db505d40d18f0feb453c
Azamat Kulov <aza.kulov@gmail.com>
2019-07-29 17:00:05 -0500

    Tugba's method commented out

--- src/test/java/tests/SmokeTest.java
+++ src/test/java/tests/SmokeTest.java
@@ -4,6 +4,7 @@ import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
